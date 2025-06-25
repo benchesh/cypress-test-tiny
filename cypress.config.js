90bfe4acf534ff929a939267f0545e380ece1aa6
Benjamin Cheshire <20292346+benchesh@users.noreply.github.com>
2025-06-25 18:33:29 +0100

    feat: demo webkit user agent bug

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) { },
   },
+  userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1",
+  experimentalWebKitSupport: true,
 }
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,5 +2,8 @@
 describe('page', () => {
   it('works', () => {
     cy.visit('https://example.cypress.io')
+
+    // fails in WebKit
+    expect(navigator.userAgent).to.eq("Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1")
   })
 })
